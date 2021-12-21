edd7f0031153c8ccb322b65fb844de28ada14945
Weaver <96252221+PythonRulz@users.noreply.github.com>
2021-12-21 12:06:12 -0600

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,27 @@
+# Exercise:
+#		prompt the user for a string and determine if the entered string is a palindrome
+#
+#
+#
+
+# Index the string and append it to a list
+# Then reverse the list and compare 
+
+def is_palindrome(string):
+    string_list = []
+    for letter in string:
+        string_list.append(letter)
+    reverse_list = string_list[::-1]
+    return string_list == reverse_list
+        
+
+def main():
+    the_string = input("Enter a string: ")
+    result = is_palindrome(the_string)
+    
+    print(
+        f"The string {the_string} is a palindrome") if result else print(
+            f"The string {the_string} is not a palindrome")
+    
+
+main()
